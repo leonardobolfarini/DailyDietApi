9f48334a094c63edb85fa7be69318ca0f0c0db7b
leonardobolfarini <leonardobolfarini@gmail.com>
2025-02-06 17:49:09 -0300

    feat: Added the new plugin and route for fastify

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import fastify from 'fastify'
 import { env } from './env'
+import fastifyCookies from '@fastify/cookie'
+import { usersRoute } from './routes/users'
 
 const app = fastify()
+
+app.register(fastifyCookies)
+app.register(usersRoute, {
+  prefix: '/users',
+})
 
 app.listen({
   port: env.PORT,
