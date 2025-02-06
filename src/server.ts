132929cb8997f21da7742c365bb9f7a992ff36a9
leonardobolfarini <leonardobolfarini@gmail.com>
2025-02-06 18:28:19 -0300

    feat: Registered the new routes to fastify app

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@ import fastify from 'fastify'
 import { env } from './env'
 import fastifyCookies from '@fastify/cookie'
 import { usersRoute } from './routes/users'
+import { mealsRoute } from './routes/meals'
 
 const app = fastify()
 
 app.register(fastifyCookies)
 app.register(usersRoute, {
   prefix: '/users',
+})
+app.register(mealsRoute, {
+  prefix: '/meals',
 })
 
 app.listen({
