989317a199f0d5ccad06f78f99f3077c9be5fffb
leonardobolfarini <leonardobolfarini@gmail.com>
2025-02-10 16:53:58 -0300

    feat: Add a route to get a user summary about how much meals, quantity in diet and out diet and the best sequence in diet

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
+import { checkUserIsRegistered } from '../middlewares/check-user-is-registered'
 
 export async function usersRoute(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
@@ -27,4 +28,54 @@ export async function usersRoute(app: FastifyInstance) {
       .status(201)
       .send()
   })
+
+  app.get(
+    '/summary',
+    { preHandler: [checkUserIsRegistered] },
+    async (request, reply) => {
+      const { userId } = request.cookies
+
+      const quantityMealsRegistered = await knex('meals')
+        .where('session_id', userId)
+        .orderBy('eated_at', 'desc')
+
+      const quantityOfInDiet = await knex('meals')
+        .where({
+          session_id: userId,
+          isInDiet: 'in',
+        })
+        .count('id', { as: 'total' })
+
+      const quantityOfOutDiet = await knex('meals')
+        .where({
+          session_id: userId,
+          isInDiet: 'out',
+        })
+        .count('id', { as: 'total' })
+
+      const { bestOnDietSequence } = quantityMealsRegistered.reduce(
+        (acc, meal) => {
+          if (meal.isInDiet === 'in') {
+            acc.currentSequence += 1
+          } else {
+            acc.currentSequence = 0
+          }
+
+          if (acc.currentSequence > acc.bestOnDietSequence) {
+            acc.bestOnDietSequence = acc.currentSequence
+          }
+
+          return acc
+        },
+        { bestOnDietSequence: 0, currentSequence: 0 },
+      )
+
+      reply.status(200).send({
+        totalMeals: quantityMealsRegistered.length,
+        quantityOfInDiet,
+        quantityOfOutDiet,
+        bestOnDietSequence,
+      })
+    },
+  )
 }
