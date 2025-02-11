c89788cc0d500d98fc2969fd96e2314a12203f9f
leonardobolfarini <leonardobolfarini@gmail.com>
2025-02-11 16:01:15 -0300

    feat: Changed the database client to the one that is set in .env file

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { Knex, knex as knexConfig } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite3',
+  client: env.DATABASE_CLIENT,
   connection: {
     filename: env.DATABASE_URL,
   },
