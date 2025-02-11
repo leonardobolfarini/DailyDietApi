a286f86d6b025d55100c4c0fbd4012adc562f22f
leonardobolfarini <leonardobolfarini@gmail.com>
2025-02-11 15:58:29 -0300

    refactor: Removed the fastify app base to another file

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,5 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from './env'
-import fastifyCookies from '@fastify/cookie'
-import { usersRoute } from './routes/users'
-import { mealsRoute } from './routes/meals'
-
-const app = fastify()
-
-app.register(fastifyCookies)
-app.register(usersRoute, {
-  prefix: '/users',
-})
-app.register(mealsRoute, {
-  prefix: '/meals',
-})
 
 app.listen({
   port: env.PORT,
