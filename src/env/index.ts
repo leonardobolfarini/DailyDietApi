391d6f3ae11bc50bff39a999cf719509a3ec1917
leonardobolfarini <leonardobolfarini@gmail.com>
2025-02-11 16:00:34 -0300

    feat: Added the path if is running test and add the database client prop if want to change database

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,7 +1,15 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
 
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
+
 const envVariablesSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
+  DATABASE_CLIENT: z.string(),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 })
